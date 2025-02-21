146ed8f72436af0e1b5f8afb6a258bc2c0c0b5af
MarinaV27 <Vasilishinamari@gmail.com>
2025-02-21 21:44:13 +0200

    add writeContacts

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,4 +1,10 @@
-import * as fs from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+    try {
+        await fs.writeFale(PATH_DB, JSON.stringify(updatedContacts, null, 2));
+    } catch (error) {
+        console.error(error.message);
+    }
+};
