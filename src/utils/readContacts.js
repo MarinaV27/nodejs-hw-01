0e9e678198e72dce0642b457487ab041f93f7fb5
MarinaV27 <Vasilishinamari@gmail.com>
2025-02-21 21:36:38 +0200

    readContacts

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,12 @@
+import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+    try {
+        const contacts = await fs.readFile(PATH_DB, { encoding: 'UTF-8' });
+        return JSON.parse(contacts);
+    } catch (error) {
+        console.error(error.message);
+        return [];
+    }
+};
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,4 @@
+import * as fs from '../constants/contacts.js';
 import { PATH_DB } from '../constants/contacts.js';
 
 export const writeContacts = async (updatedContacts) => {};
