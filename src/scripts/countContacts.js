4269255d7980acae4e993c674141348d2b1f9190
MarinaV27 <Vasilishinamari@gmail.com>
2025-02-22 00:49:18 +0200

    countContacts

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,8 @@
-export const countContacts = async () => {};
+import { readContacts } from "../utils/readContacts.js";
+
+export const countContacts = async () => { 
+    const contactsLength = await readContacts();
+    return contactsLength.length;
+};
 
 console.log(await countContacts());
