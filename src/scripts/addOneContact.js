232a1b57d378842419cc47994ee5c95ed50745dd
MarinaV27 <Vasilishinamari@gmail.com>
2025-02-22 00:36:42 +0200

    addOneContact

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,21 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from "../utils/createFakeContact.js";
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+
+
+
+export const addOneContact = async () => { 
+    try {
+    const contacts = await readContacts();
+    const newContacts = createFakeContact();
+        const saveContacts = [...contacts, newContacts];
+        await writeContacts(saveContacts);
+        
+  } catch (error) {
+    console.error('Error generating contacts:', error);
+  }
+};
+
+
 
 addOneContact();
